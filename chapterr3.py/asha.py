ba1a31156e5d2f3ba31e8d63f3a64da7770795ba
asha <lgh226407@gmail.com>
2026-01-12 17:41:00 +0900

    gg

--- chapterr3.py/asha.py
+++ chapterr3.py/asha.py
@@ -1,3 +1,34 @@
-a, b, c = input().split()
-A = int(a)+int(b)+int(c)
-print(A)
+# a, b, c = input().split()
+# A = int(a)+int(b)+int(c)
+# print(A)
+
+
+# a = input()
+# b = input()
+# c = input()
+# A = int(a)+int(b)+int(c)
+# print(A)
+
+
+# w, h, a = input().split()
+# w = int(w) 
+# h = int(h)
+# a = int(a)
+# print(66.47 +(13.75+w)+(5 * h)-(6.76 * a))
+
+
+
+# a = int(input())
+# if a >= 80:
+#     print("합격")
+
+
+# a = input()
+# if a[0] == "a":
+#     print(len(a))
+
+a = int(input())
+if a <= 255:
+    print(a)
+else:
+    print(255)
