55b2799631a5fde6aae03e640a5cbd733d7a7fc6
asha <lgh226407@gmail.com>
2026-01-12 16:04:13 +0900

    first commit

---
+++ chapterr3.py/실습.py
@@ -0,0 +1,76 @@
+#n = [-32, 75, 97, -10, 9, 32, 4, -15, 0, 76, 14, 2]
+#print(n[1: : 2])
+
+
+
+#a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#print(a[::-1])
+
+
+
+
+#date = "20251231"
+#print(f"{date[0:4]}년 {date[4:6]}월 {date[6:]}일")
+
+
+#nums = [1, 2, 3, 4, 5]
+#print( (nums[0]+nums[1]+nums[2]+nums[3]+nums[4])/len(nums))
+
+
+#a = [1, 2, ['a', 'b', ['Life', 'is']]]
+#print(a[2][2][0])
+
+
+
+#string = "삼성전자/naver/포스코"
+#a = [string[0:4], string[5:10], string[11:]]
+#print(a)
+
+
+
+
+#movie_rank = ['닥터 스트레인지', '스플릿', '럭키', '배트맨']
+#movie_rank.insert(1, "슈퍼맨")
+#print(movie_rank)
+
+
+
+
+#A, B = input().split()
+#print(int(A)+int(B))
+#print(int(A)-int(B))
+#print(int(A)*int(B))
+#print(int(A)//int(B))
+#print(int(A)%int(B))
+
+
+
+
+
+#t = ('A', 'b', 'c')
+#t = ('a', 'b', 'c')
+#print(t)
+
+
+
+
+#t = ('1등 오펜하이머', '2등 콘크리트', '3등 달짝지근해')
+#print(t)
+
+
+
+#key = {'메로나': [1000, 5], '돼지바': [800, 7], '월드콘': [1500, 4]}
+#print(key['메로나'][1])
+
+
+
+
+
+#print(28%4==0)
+#print(28%7==0)
+#print(28%4==0 and 28%7==0)
+
+#a, b = input().split()
+#a = int(a)
+#b = int(b)
+#print((a%2==0 and b%2==1) or (a%2==1 and b%2==0))
